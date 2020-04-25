18c6983ab000a38e959286a827e249e10d63b0e8
Sean <seanmkelley11@gmail.com>
2020-04-25 11:30:12 +0200

    fixes and a new potential

--- schrodinger.js
+++ schrodinger.js
@@ -2,8 +2,15 @@
 // Runge-Kutta method (RK4) for approximating the evolution of the time-dependent Schrodinger equation
 //
 // To Do:
-// - Figure out why the wavepacket isn't moving. The phase is cycling as expected, but the peak doesn't move
-// - Figure out how to reuse functions between shaders...right now there is a lot of duplication 
+// - Figure out how to reuse functions between shaders...right now there is a lot of duplication. Would like to be able to define an initial wavefunction and potential externally
+// - Switch to using a texture for the potential (so we can define custom textures by drawing them, for example)
+//    - Scattering off a point
+//    - Single slit, Double slit
+//    - Reflection / Transmission (Tunneling)
+//    - Exponential well (harmonic oscillator)
+//    - 3D set energy (above potential) see how hole affects propagation
+// - Arbitrary initial position
+// - Arbitrary initial momentum (choose direction)
 // ------
 
 //using regl, a functional wrapper for webgl, to make everything simpler
@@ -35,7 +42,9 @@ let k4Buffer      = makeFrameBuffer();
 let kCombined     = makeFrameBuffer();
 
 // time step size
-const dt = 0.05;
+const dt = 0.25;
+// space step size
+const dx = 1.0 / 256.0;
 
 const create_texture = regl({
   uniforms: { // inputs to the shader
@@ -60,12 +69,12 @@ const create_texture = regl({
     // Initial wavefunction, nondispersive packet with some arbitrary values (we can adjust later)
 
     #define k 100.0 // Frequency
-    #define sigma 50.0 // Width of the envelope
+    #define sigma 70.0 // Width of the envelope
     #define length2(p) dot(p,p)
 
     vec2 initial_wavefunction(vec2 p) {
       // the function returns a vec2 where the first component is real and the second is imaginary
-      return exp(-sigma * length2(p - vec2(0.5, 0.5))) * vec2(cos(k * (p.x)),  sin(k * (p.x)));
+      return exp(-sigma * length2(p - vec2(0.25, 0.25))) * vec2(cos(k * (p.x + p.y)),  sin(k * (p.x + p.y)));
     }
 
     // The approximated wavefunction evolution, which we switch to after the first step
@@ -100,7 +109,8 @@ const k1 = regl({
   uniforms: {
     u_resolution: ctx => [ctx.framebufferWidth,ctx.framebufferHeight],
     texture: initialBuffer,
-    dt : dt 
+    dt : dt,
+    dx: dx
   },
   
   vert:`
@@ -116,6 +126,7 @@ const k1 = regl({
     uniform sampler2D k_combined_texture;
     uniform vec2 u_resolution;
     uniform float dt;
+    uniform float dx;
 
     #define initial_wavefunction(p) texture2D(texture, p).xy
     #define psi(p) (initial_wavefunction(p))
@@ -127,17 +138,17 @@ const k1 = regl({
 
     // define square well potential, returns 1.0 at the edges and 0.0 everwhere else
     float potential(vec2 p) {
-      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01);
+      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01) + 0.5 * float(p.y < 0.505 && p.y > 0.495);
     }
 
     // compute the first "intermediate" wavefunction psi-k1
     vec2 k1(vec2 p) {
       // calculate each component of the approximated laplacian
       vec2 psi_initial = psi(p);
-      vec2 psi_y_inc = psi(p + vec2(0,1));
-      vec2 psi_x_inc = psi(p + vec2(1,0));
-      vec2 psi_y_dec = psi(p - vec2(0,1));
-      vec2 psi_x_dec = psi(p - vec2(1,0));
+      vec2 psi_y_inc = psi(p + vec2(0,dx));
+      vec2 psi_x_inc = psi(p + vec2(dx,0));
+      vec2 psi_y_dec = psi(p - vec2(0,dx));
+      vec2 psi_x_dec = psi(p - vec2(dx,0));
       // combine the components
       vec2 laplacian = psi_y_inc + psi_x_inc + psi_y_dec + psi_x_dec - (4.0 * psi_initial);
       // return the intermediate wavefunction psi-k1
@@ -166,7 +177,8 @@ const k2 = regl({
     u_resolution: ctx => [ctx.framebufferWidth,ctx.framebufferHeight],
     k1_texture: k1Buffer,
     texture: initialBuffer,
-    dt : dt
+    dt : dt,
+    dx: dx
   },
   
   vert:`
@@ -182,6 +194,7 @@ const k2 = regl({
     uniform sampler2D k1_texture;
     uniform vec2 u_resolution;
     uniform float dt;
+    uniform float dx;
 
     #define wavefunction(p) texture2D(texture, p).xy
     #define k1(p) texture2D(k1_texture, p).xy
@@ -192,15 +205,15 @@ const k2 = regl({
     }
 
     float potential(vec2 p) {
-      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01);
+      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01) + 0.5 * float(p.y < 0.505 && p.y > 0.495);
     }
 
     vec2 k2(vec2 p) {
       vec2 psi_initial = psi(p);
-      vec2 psi_y_inc = psi(p + vec2(0,1));
-      vec2 psi_x_inc = psi(p + vec2(1,0));
-      vec2 psi_y_dec = psi(p - vec2(0,1));
-      vec2 psi_x_dec = psi(p - vec2(1,0));
+      vec2 psi_y_inc = psi(p + vec2(0,dx));
+      vec2 psi_x_inc = psi(p + vec2(dx,0));
+      vec2 psi_y_dec = psi(p - vec2(0,dx));
+      vec2 psi_x_dec = psi(p - vec2(dx,0));
       vec2 laplacian = psi_y_inc + psi_x_inc + psi_y_dec + psi_x_dec - (4.0 * psi_initial);
       return divide_by_sqrt_neg_one(-laplacian + potential(p) * psi_initial);
     }
@@ -227,7 +240,8 @@ const k3 = regl({
     u_resolution: ctx => [ctx.framebufferWidth,ctx.framebufferHeight],
     k2_texture: k2Buffer,
     texture: initialBuffer,
-    dt : dt
+    dt : dt,
+    dx: dx
   },
   
   vert:`
@@ -243,6 +257,7 @@ const k3 = regl({
     uniform sampler2D k2_texture;
     uniform vec2 u_resolution;
     uniform float dt;
+    uniform float dx;
 
     #define wavefunction(p) texture2D(texture, p).xy
     #define k2(p) texture2D(k2_texture, p).xy
@@ -253,15 +268,15 @@ const k3 = regl({
     }
 
     float potential(vec2 p) {
-      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01);
+      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01) + 0.5 * float(p.y < 0.505 && p.y > 0.495);
     }
 
     vec2 k3(vec2 p) {
       vec2 psi_initial = psi(p);
-      vec2 psi_y_inc = psi(p + vec2(0,1));
-      vec2 psi_x_inc = psi(p + vec2(1,0));
-      vec2 psi_y_dec = psi(p - vec2(0,1));
-      vec2 psi_x_dec = psi(p - vec2(1,0));
+      vec2 psi_y_inc = psi(p + vec2(0,dx));
+      vec2 psi_x_inc = psi(p + vec2(dx,0));
+      vec2 psi_y_dec = psi(p - vec2(0,dx));
+      vec2 psi_x_dec = psi(p - vec2(dx,0));
       vec2 laplacian = psi_y_inc + psi_x_inc + psi_y_dec + psi_x_dec - (4.0 * psi_initial);
       return divide_by_sqrt_neg_one(-laplacian + potential(p) * psi_initial);
     }
@@ -288,7 +303,8 @@ const k4 = regl({
     u_resolution: ctx => [ctx.framebufferWidth,ctx.framebufferHeight],
     k3_texture: k3Buffer,
     texture: initialBuffer,
-    dt : dt
+    dt : dt,
+    dx: dx
   },
   
   vert:`
@@ -304,6 +320,7 @@ const k4 = regl({
     uniform sampler2D k3_texture;
     uniform vec2 u_resolution;
     uniform float dt;
+    uniform float dx;
 
     #define wavefunction(p) texture2D(texture, p).xy
     #define k3(p) texture2D(k3_texture, p).xy
@@ -314,15 +331,15 @@ const k4 = regl({
     }
 
     float potential(vec2 p) {
-      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01);
+      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01) + 0.5 * float(p.y < 0.505 && p.y > 0.495);
     }
 
     vec2 k4(vec2 p) {
       vec2 psi_initial = psi(p);
-      vec2 psi_y_inc = psi(p + vec2(0,1));
-      vec2 psi_x_inc = psi(p + vec2(1,0));
-      vec2 psi_y_dec = psi(p - vec2(0,1));
-      vec2 psi_x_dec = psi(p - vec2(1,0));
+      vec2 psi_y_inc = psi(p + vec2(0,dx));
+      vec2 psi_x_inc = psi(p + vec2(dx,0));
+      vec2 psi_y_dec = psi(p - vec2(0,dx));
+      vec2 psi_x_dec = psi(p - vec2(dx,0));
       vec2 laplacian = psi_y_inc + psi_x_inc + psi_y_dec + psi_x_dec - (4.0 * psi_initial);
       return divide_by_sqrt_neg_one(-laplacian + potential(p) * psi_initial);
     }
@@ -421,14 +438,14 @@ const draw_frame = regl({
     #define hue2rgb(h) clamp(abs(mod(6.*(h)+vec3(0,4,2),6.)-3.)-1.,0.,1.)
 
     float potential(vec2 p) {
-      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01);
+      return float(p.y > 0.99 || p.y < 0.01 || p.x > 0.99 || p.x < 0.01) + 0.5 * float(p.y < 0.505 && p.y > 0.495);
     }
 
     void main () {
       vec2 st = gl_FragCoord.xy / u_resolution;
       vec2 v = k_combined(st);
-      gl_FragColor = vec4(1.5 * length(v) * hue2rgb(atan(v.y,v.x)/(2.*PI)) +
-             0.25*potential(st), 1.0)
+      // gl_FragColor = vec4(1.5 * length(v) * hue2rgb(atan(v.y,v.x)/(2.*PI)) + 0.25*potential(st), 1.0) + vec4(potential(st), potential(st), potential(st), 1.0);
+      gl_FragColor = vec4(0.0, v, 1.0)
                      + vec4(potential(st), potential(st), potential(st), 1.0);
     }`,
 
@@ -442,7 +459,7 @@ const draw_frame = regl({
   count: 3,
 });
 
-const animationTickLimit = 1000; // -1 disables
+const animationTickLimit = 2000; // -1 disables
 
 if (animationTickLimit >= 0) {
   console.log(`Limiting to ${animationTickLimit} ticks`);
